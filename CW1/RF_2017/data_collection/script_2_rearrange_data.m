ea0ae5088bd440b84f5f08729b7f7a34f6e42d0e
pranavmalhotra <pranav.malhotra13@imperial.ac.uk>
2017-03-14 11:42:39 +0000

    updated splitNum

--- CW1/RF_2017/data_collection/script_2_rearrange_data.m
+++ CW1/RF_2017/data_collection/script_2_rearrange_data.m
@@ -1,6 +1,6 @@
 numBins = 256;
-treeNum = [10,50,150];
-splitNum = [3,5,7,9,11];
+treeNum = [10,150];
+splitNum = [3,5,7,10,12,14,16,18,20,25,30,35,40,50,100,150];
 learner_t = {'axis-aligned', 'two-pixel'};
 
 accuracy_rf_all=zeros(numel(treeNum), numel(splitNum), numel(learner_t));
@@ -13,7 +13,7 @@ for index=1:numel(treeNum)
             learner_type = learner_t{kindex};
             numTrees = treeNum(index);
             split = splitNum(jindex);
-            filename =  sprintf('data_collection/kmean_learner_split_accuracy_3/numbins_%d/%d_splitnum_%d_learner_%d_treenum.mat', numBins,split,kindex,numTrees);
+            filename =  sprintf('data_collection/learner_split/numbins_%d/%d_splitnum_%d_learner_%d_treenum.mat', numBins,split,kindex,numTrees);
             load(filename);
             accuracy_rf_all(index, jindex, kindex)=accuracy_rf;
             buildCodebookTime_all(index, jindex, kindex)=buildCodebookTime;      
