619dca1d7c0c75a4abf39e067ec3863ba3b6a47c
pranavmalhotra <pranav.malhotra13@imperial.ac.uk>
2017-03-14 10:15:58 +0000

    added times for splitNum

--- CW1/RF_2017/data_collection/script_2_rearrange_data.m
+++ CW1/RF_2017/data_collection/script_2_rearrange_data.m
@@ -1,10 +1,11 @@
 numBins = 256;
 treeNum = [10,50,150];
 splitNum = [3,5,7,9,11];
-learner_t = {'axis-aligned', 'two-pixel', 'quad-features-x-axis-only', 'linear', 'cube-features-x-axis-only'};
+learner_t = {'axis-aligned', 'two-pixel'};
 
 accuracy_rf_all=zeros(numel(treeNum), numel(splitNum), numel(learner_t));
 buildCodebookTime_all=zeros(numel(treeNum), numel(splitNum), numel(learner_t));
+buildRfClassifierTime_all=zeros(numel(treeNum), numel(splitNum), numel(learner_t));
 
 for index=1:numel(treeNum)
     for jindex=1:numel(splitNum)
@@ -15,9 +16,10 @@ for index=1:numel(treeNum)
             filename =  sprintf('data_collection/kmean_learner_split_accuracy_3/numbins_%d/%d_splitnum_%d_learner_%d_treenum.mat', numBins,split,kindex,numTrees);
             load(filename);
             accuracy_rf_all(index, jindex, kindex)=accuracy_rf;
-            buildCodebookTime_all(index, jindex, kindex)=buildCodebookTime;        
+            buildCodebookTime_all(index, jindex, kindex)=buildCodebookTime;      
+            buildRfClassifierTime_all(index, jindex, kindex)=buildRFTime; 
         end
     end
 end
 
-clearvars -except buildCodebookTime_all accuracy_rf_all splitNum treeNum numBins learner_t
+clearvars -except buildCodebookTime_all accuracy_rf_all splitNum treeNum numBins learner_t buildRfClassifierTime_all
