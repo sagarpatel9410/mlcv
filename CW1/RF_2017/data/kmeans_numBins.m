e95ce64caf3ebe5432d455942d937417f09b6ae9
pranavmalhotra <pranav.malhotra13@imperial.ac.uk>
2017-03-14 11:08:36 +0000

    added functions to plot correctly and wrongly classified images

--- CW1/RF_2017/data/kmeans_numBins.m
+++ CW1/RF_2017/data/kmeans_numBins.m
@@ -2,7 +2,7 @@ load kmeans_codebook_accuracy.mat
 
 figure(1)
 plot(numBin, accuracy_rf_all, 'LineWidth', 2);
-axis([10 1024 0 1]);
+axis([5 1024 0 1]);
 set(gca,'fontsize',20);
 grid on; grid minor;
 title('Impact of Vocabulary Size on Accuracy','FontSize',40);
@@ -12,11 +12,11 @@ legend({'Trees=10', 'Trees=50', 'Trees=150'}, 'FontSize', 25);
 scale_graph;
 
 figure(2)
-plot(numBin, buildCodebookTime_all, 'LineWidth', 2);
-axis([10 1024 0 200]);
+plot(numBin, mean(buildCodebookTime_all), 'LineWidth', 2);
+axis([5 1024 0 200]);
 set(gca,'fontsize',20);
 grid on; grid minor;
-title('Computational Complexity of Vocabulary Size','FontSize',40);
+title('Complexity of Vocabulary Size','FontSize',40);
 xlabel('Number of Bins', 'FontSize', 25);
 ylabel('Time Taken to Build Kmeans Codebook (s)', 'FontSize', 25);
 scale_graph;
